fd39b678f33cb884e02685dfecd1b6b368964210
unknown <cantatnathan@gmail.com>
2020-11-10 22:43:48 +0100

    Added a new way of matching strings

--- main.py
+++ main.py
@@ -10,10 +10,11 @@ import os
 import srt
 import subprocess
 import datetime
-from fuzzywuzzy import fuzz
+from fuzzywuzzy import fuzz, process
 from bs4 import BeautifulSoup
 from glob import glob
-
+from fuzzysearch import find_near_matches
+from datetime import datetime
 
 
 def get_dialogs(html_link):
@@ -59,18 +60,67 @@ def extract_subtitles(filename):
     #print(len(list(subtitles)))
     return subtitles
 
-def search_subtitle(subtitles, line):
+def search_subtitle(subtitles, line, offset):
     i = 0;
+    subtitleslist = []
+    for sub in subtitles:
+        subtitleslist.append(sub.content)
+    best = process.extractOne(line, subtitleslist)
+    if best[1] > 90:
+        for sub in subtitles:
+            if sub.content == best[0]:
+                return i
+            i+=1
+        
+    
+    i=0
     for sub in subtitles:
         
         if fuzz.ratio(str(sub.content).lower(), line.lower()) > 80:
             #print(line, sub, str(fuzz.ratio(str(sub.content).lower(), line.lower())))
             return i
         elif fuzz.partial_ratio(str(sub.content).lower(), line.lower()) > 80:
+            if fuzz.partial_ratio(str(subtitles[i+offset].content).lower(), line.lower()) > 80: 
+                #If lines are split between multiple subtitles
+                return i+offset
             return i
         i+=1
     return -1
-        
+
+def search_subtitle_2(subtitles, line, offset):
+    allsub = ""
+    partialsum = []
+    for sub in subtitles:
+        allsub = allsub + sub.content + " "
+        partialsum.append(len(allsub))
+    
+    line = line.lower()
+    allsub = allsub.lower()
+    t1 = datetime.now()
+    result = find_near_matches(line, allsub, max_l_dist=5)
+    t2 = datetime.now()
+    print((t2-t1).seconds)
+    if len(result) != 1:
+        return -1
+    
+    result = result[0]
+    print(result)
+    if offset > 0: #Searching for after
+        #Searching for the index of the end of the substring
+        i = 0
+        for val in partialsum:
+            if val > result.end:
+                return i
+            i+=1
+
+    else: #Searching for before
+     #Searching for the index of the begining of the substring   
+         i=len(partialsum)
+         for val in reversed(partialsum):
+             if val < result.start:
+                 return i+1
+             i-=1
+    return -1
 
 def get_kenny_lines(text):
     lines = []
@@ -80,6 +130,7 @@ def get_kenny_lines(text):
             lines.append(i)
             line[1] = line[1].replace("\\", "")
         i+=1
+        
     print(str(len(lines)) + " kenny lines found")
     return lines
 
@@ -95,6 +146,11 @@ def replace_line(dialogs, ind, line, subtitles):
     else:
         end_time = subtitles[ind].end + datetime.timedelta(0,2)
     
+    duration = end_time-start_time
+    if duration.microseconds <= 125000:
+        print("Subtitles shorter than 125 ms ({})".format(duration.microseconds/1000.0), dialogs[line][1])
+        print("At time " + str(start_time))
+    
     sub = srt.Subtitle(ind + 2, start_time, end_time, dialogs[line][1])
     
     for i in range(ind + 1, len(subtitles)): #Moving all subtitles back
@@ -106,61 +162,35 @@ def replace_line(dialogs, ind, line, subtitles):
 def insert_kenny_lines(dialogs, subtitles):
     lines = get_kenny_lines(dialogs)
     #lines.reverse()
+    maxInd = 0
     for line in lines:
         #print("Searching line " + str(line))
         found = False
-        offset = 1
-        
+        offset = 0
+
         while not found and (line >= offset or line + offset < len(dialogs)): #Search for string before and after
         #End the while when all possibilities have been depleted
             if line >= offset:
-                ind = search_subtitle(subtitles, dialogs[line-offset-1][1]) #Search the subtitles for the lines before
-                if ind > 0:
+                ind2 = search_subtitle(subtitles, dialogs[line-offset-1][1], 1) #Search the subtitles for the lines before
+                ind = search_subtitle_2(subtitles, dialogs[line-offset-1][1], 1)
+                #print(search_subtitle_2(subtitles, dialogs[line-offset-1][1], 1), ind)  
+                if ind > 0 and ind > maxInd:
                     subtitles = replace_line(dialogs, ind + offset, line, subtitles)
                     found = True
+                    maxInd = ind
             
             if line + offset + 1 < len(dialogs) and not found:
-                ind = search_subtitle(subtitles, dialogs[line+offset-1][1]) #Search the subtitles for the lines after
-                if ind > 0:
-                    subtitles = replace_line(dialogs, ind - offset, line, subtitles)
-                    found = True
-            
-            
+                    ind2 = search_subtitle(subtitles, dialogs[line+offset+1][1], -1) #Search the subtitles for the lines after
+                    ind = search_subtitle_2(subtitles, dialogs[line+offset+1][1], -1)
+                    #print(search_subtitle_2(subtitles, dialogs[line+offset+1][1], -1), ind)
+                    if ind > 0 and ind > maxInd:
+                        subtitles = replace_line(dialogs, ind - offset + 1, line, subtitles)
+                        found = True
+                        maxInd = ind
             offset+=1
             
         if not found:
             print("Could not find line : " + dialogs[line][1])
-        '''
-        ind = search_subtitle(subtitles, dialogs[line-1][1]) #Search the subtitles for the line just before
-
-        if ind < 0 and len(dialogs) > line: #To prevent out of range
-            ind = search_subtitle(subtitles, dialogs[line+1][1]) #Search the subtitles for the line just after
-            if ind < 0:
-                print("Could not find line : " + dialogs[line][1])
-                pass
-            else:
-                #print("Line found", dialogs[line][1])
-                start_time = subtitles[ind - 1].end
-                end_time = subtitles[ind].start
-                
-                sub = srt.Subtitle(ind + 1, start_time, end_time, dialogs[line][1])
-                
-                for i in range(ind, len(subtitles)): #Moving all subtitles back
-                    subtitles[i].index+=1
-                
-                subtitles.insert(ind, sub)
-        elif ind > 0:
-            #print("Line found", dialogs[line][1])
-            start_time = subtitles[ind].end
-            end_time = subtitles[ind + 1].start
-            
-            sub = srt.Subtitle(ind+2, start_time, end_time, dialogs[line][1])
-            
-            for i in range(ind+1, len(subtitles)): #Moving all subtitles back
-                subtitles[i].index+=1
-                
-            subtitles.insert(ind+1, sub)
-        '''
     return subtitles
 
 def rewrite_subtitles(new_sub, filename, output):
@@ -205,15 +235,39 @@ def get_links():
         
     return links
 
+def check_subtitles(text):
+    file = open("temp/subtitles.srt", "r")
+    lines = file.readlines()
+    file.close()
+    klines = get_kenny_lines(text)
+    found = True
+    not_found = []
+    for kline in klines:
+        found = False
+        for line in lines:
+            if fuzz.ratio(text[kline][1], line) > 90:
+                found = True
+                break
+        if not found:
+            not_found.append(kline)
+            
+    if len(not_found) == 0:
+        return True
+    else:
+        print("Kenny subtitles not found " + str(len(klines)-len(not_found)) + "/" + str(len(klines)))
+        print(not_found)
+        return False
+
 links = get_links()
 
-"""
-dialogs = get_dialogs(links[0][3])
-subs = extract_subtitles("South Park S01E04 MULTi 1080p BluRay x264-GLaDOS.mkv")
+
+dialogs = get_dialogs(links[0][2])
+subs = extract_subtitles("South Park S01E03 MULTi 1080p BluRay x264-GLaDOS.mkv")
 subs_array = list(subs)
 new_subs = insert_kenny_lines(dialogs, subs_array)
-rewrite_subtitles(new_subs, "South Park S01E04 MULTi 1080p BluRay x264-GLaDOS.mkv", "out/South Park S01E04 MULTi 1080p BluRay x264-GLaDOS.mkv")
-"""
+rewrite_subtitles(new_subs, "South Park S01E03 MULTi 1080p BluRay x264-GLaDOS.mkv", "out/South Park S01E03 MULTi 1080p BluRay x264-GLaDOS.mkv")
+check_subtitles(dialogs)
+
 i=0
 for folder in glob("Y:\Series\South Park S01-S21 MULTi 1080p BluRay x264-GLaDOS/*/"):
     fol = folder[3:]
@@ -230,6 +284,7 @@ for folder in glob("Y:\Series\South Park S01-S21 MULTi 1080p BluRay x264-GLaDOS/
         new_subs = insert_kenny_lines(dialogs, subs_array)
         filelist = file.split("\\")
         rewrite_subtitles(new_subs, file, "Y:\Series/out/" + filelist[3] + "\\" + filelist[4])
+        check_subtitles(dialogs)
         j+=1
     i+=1
 
